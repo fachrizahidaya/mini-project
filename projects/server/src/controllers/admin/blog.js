22527c1f3b40c984c6da135dcaa3b801ceecf23a
fachrizahidaya <fachriza.theblues@gmail.com>
2023-04-13 18:59:02 +0700

    updated

--- projects/server/src/controllers/admin/blog.js
+++ projects/server/src/controllers/admin/blog.js
@@ -36,27 +36,47 @@ module.exports = {
 
   pagBlog: async (req, res) => {
     try {
-      const page = parseInt(req.query.page) - 1 || 0;
-      const limit = parseInt(req.query.limit) || 5;
-      const search = req.query.search || "";
-      let sort = req.query.sort || "date";
-      let genre = req.query.genre || "All";
-
-      const genreOption = await category.findAll({
-        attributes: ["name"],
-      });
-      genre === "All"
-        ? (genre = [...genreOption])
-        : (genre = req.query.genre.split(","));
-      req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
-
-      let sortBy = {};
-      if (sort[1]) {
-        sortBy[sort[0]] = sort[1];
-      } else {
-        sortBy[sort[0]] = "ASC";
+      const pageAsNumber = Number.parseInt(req.query.page);
+      const sizeAsNumber = Number.parseInt(req.query.size);
+      let page = 0;
+      if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+        page = pageAsNumber;
       }
+      let size = 5;
+      if (!Number.isNaN(sizeAsNumber) && sizeAsNumber < 10) {
+        size = size;
+      }
+
+      const data = await blog.findAndCountAll({
+        limit: size,
+        offset: page * size,
+      });
+      // const page = parseInt(req.query.page) - 1 || 0;
+      // const limit = parseInt(req.query.limit) || 5;
+      // const search = req.query.search || "";
+      // let sort = req.query.sort || "date";
+      // let genre = req.query.genre || "All";
+
+      // const genreOption = await category.findAll({
+      //   attributes: ["name"],
+      // });
+      // genre === "All"
+      //   ? (genre = [...genreOption])
+      //   : (genre = req.query.genre.split(","));
+      // req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+
+      // let sortBy = {};
+      // if (sort[1]) {
+      //   sortBy[sort[0]] = sort[1];
+      // } else {
+      //   sortBy[sort[0]] = "ASC";
+      // }
+      res.status(200).send({
+        content: data.rows,
+        totalPage: Math.ceil(data.count / size),
+      });
     } catch (err) {
+      console.log(err)
       res.status(400).send(err);
     }
   },
--- projects/server/src/routers/admin/blog.js
+++ projects/server/src/routers/admin/blog.js
@@ -3,5 +3,6 @@ const { blogAdmin } = require("../../controllers/index");
 
 router.get("/allBlog", blogAdmin.allBlog);
 router.get("/allCategory", blogAdmin.allCategory);
+router.get("/pagBlog", blogAdmin.pagBlog);
 
 module.exports = router;
