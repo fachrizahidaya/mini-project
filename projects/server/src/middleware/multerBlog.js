d9a04d3ab5df375593086895ca9e541512ef1cfc
fachrizahidaya <fachriza.theblues@gmail.com>
2023-05-02 09:17:56 +0700

    updated

--- projects/server/src/middleware/multerBlog.js
+++ projects/server/src/middleware/multerBlog.js
@@ -1,10 +1,6 @@
 const multer = require("multer");
 const path = require("path");
 
-// Masukin gambarnya za bikin multer baru ajh, di ubah nama filenya .. pake blog-{id} biar ga numpuk file.
-
-// Jadi kmaren knpa di ganti ke avatar-id .. karna kalau ada user ganti foto baru .. di publicnya jadi ngeganti foto yang lama .. bukan nambahin, karena nama file nya sama .. avatar-1.jpg nah jadinya replace
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "../Public"));
