eb452d65ff8d7cf6f2f0bc138584f7b498f34baf
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-05-05 16:30:05 +0700

    updated

--- projects/server/src/controllers/user/blog.js
+++ projects/server/src/controllers/user/blog.js
@@ -14,8 +14,7 @@ const youtubeThumbnail = require(local);
 module.exports = {
   create: async (req, res) => {
     try {
-      const { title, content, CategoryId, url, keywords } = req.body;
-      // const key1 = parseInt(id_key);
+      const { title, content, CategoryId, url, keywords, country } = req.body;
       const allowedTypes = [
         "image/jpg",
         "image/jpeg",
@@ -45,7 +44,6 @@ module.exports = {
       if (!content) throw `Content must not be empty`;
       if (!CategoryId) throw `Category must not be empty`;
 
-      // id keyword yang mau masuk 5,6
       const t = await db.sequelize.transaction();
       try {
         var result = await blog.create(
@@ -56,6 +54,7 @@ module.exports = {
             CategoryId: CategoryId,
             imageURL: `Public/${fileUploaded?.filename}`,
             videoURL: url,
+            country: country,
           },
           {
             transaction: t,
@@ -179,7 +178,7 @@ module.exports = {
           "videoURL",
           "UserId",
           "CategoryId",
-          "createdAt"
+          "createdAt",
         ],
         include: [
           { model: category, attributes: ["name"] },
@@ -210,7 +209,7 @@ module.exports = {
           "videoURL",
           "UserId",
           "CategoryId",
-          "createdAt"
+          "createdAt",
         ],
         include: [
           { model: category, attributes: ["name"] },
@@ -312,7 +311,7 @@ module.exports = {
           ],
         },
       });
-      console.log(totalRows);
+      
       const totalPage = Math.ceil(totalRows / size1);
       res.status(200).send({
         page: totalPage,
@@ -478,7 +477,7 @@ module.exports = {
       });
     } catch (err) {
       res.status(400).send(err);
-      console.log(err);
+      
     }
   },
 
@@ -614,9 +613,10 @@ module.exports = {
                 model: keyword,
                 where: {
                   name: {
-                    [Op.like]: `%${search}%`,
+                    [Op.like]: `%${search1}%`,
                   },
                 },
+                required: false,
               },
             ],
             required: false,
@@ -732,96 +732,6 @@ module.exports = {
         listLimit: size1,
         result,
       });
-    } catch (err) {
-      res.status(400).send(err);
-    }
-  },
-
-  createKey: async (req, res) => {
-    try {
-      const { name } = req.body;
-      const data = await keyword.create({
-        name: name,
-      });
-      res.status(200).send(data);
-    } catch (err) {
-      res.status(400).send(err);
-    }
-  },
-
-  save: async (req, res) => {
-    try {
-      const { id_cat, search, sort, size, id_key } = req.query;
-      const cat1 = id_cat;
-      const idKey1 = id_key;
-      const sort1 = sort || "DESC";
-      const page1 = parseInt(req.query.page) + 1 || 1;
-      const size1 = parseInt(size) || 8;
-      const search1 = search || "";
-      const start = (page1 - 1) * size1;
-      const blogs = await blogKeyword.findAll({
-        include: [
-          {
-            model: blog,
-            include: [
-              {
-                model: user,
-                attributes: ["id", "username"],
-              },
-              {
-                model: category,
-                attributes: ["id", "name"],
-              },
-            ],
-            where: {
-              title: {
-                [Op.like]: "%%",
-              },
-              CategoryId: {
-                [Op.like]: "%%",
-              },
-            },
-            order: [["createdAt", "DESC"]],
-          },
-          {
-            model: keyword,
-            attributes: ["id", "name"],
-            where: {
-              id: {
-                [Op.like]: "%%",
-              },
-            },
-          },
-        ],
-        attributes: ["id"],
-        limit: 8,
-      });
-
-      const blogsIds = blogs.map((blog) => blog.BlogId);
-
-      const result = await blog.findAll({
-        where: {
-          id: {
-            [Op.in]: blogsIds,
-          },
-        },
-        include: [
-          {
-            model: user,
-            attributes: ["id", "username"],
-          },
-          {
-            model: category,
-            attributes: ["id", "name"],
-          },
-          {
-            model: keyword,
-            attributes: ["id", "name"],
-          },
-        ],
-        order: [["createdAt", "DESC"]],
-      });
-      res.status(200).send("success");
     } catch (err) {
       res.status(400).send(err);
     }
